9311f822155852b5446136a58ea1b9a870ce23bf
saketh <uday.kundoor123@gmail.com>
2024-03-03 16:09:39 +0530

    edited redirectors

--- mysql.sh
+++ mysql.sh
@@ -5,10 +5,10 @@ dnf install mysql-server -y &>>/tmp/expense.log
 echo $?
 
 Print_Heading "Start MySQL Service"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>/tmp/expense.log
+systemctl start mysqld &>>/tmp/expense.log
 echo $?
 
 Print_Heading "Change root pwd"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
 echo $?
